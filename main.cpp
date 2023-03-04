30f98adfbb8330d820e9288cf5e1492c1821859f
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-03-04 17:04:23 +0100

    Use tests in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include "list/IntegerDoubleSidedList.h"
-#include "exception/Exception.h"
+#include "Tests.h"
 
 using namespace std;
-using namespace SDIZO;
 
 int main (int argc, char* argv[]) {   
-    IntegerDoubleSidedList* list = new IntegerDoubleSidedList();
-    
-    for(int i = 0 ; i < 5000; i++) {
-        list->pushBack(i);
-    }
+    testDoubleSidedList();
 
     return 0;
 } 
