f526dc0d93069affc3e7eac8b4fb6f3825ac26cb
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-03-10 19:34:55 +0100

    Add random number generator class

---
+++ include/utils/RandomNumberGenerator.h
@@ -0,0 +1,13 @@
+#include <random>
+
+namespace SDIZO {
+    class RandomNumberGenerator {
+    private:
+        std::mt19937 generator;
+        std::uniform_int_distribution<>* dist;
+    public:
+        RandomNumberGenerator(int min = 0, int max = 1000000);
+        ~RandomNumberGenerator();
+        int nextInt();
+    };
+}
