79428e13f3db2733ad0e00fff0f4a3bdae9bd852
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2023-04-15 15:39:40 +0200

    Increase range of generated numbers

--- include/utils/RandomNumberGenerator.h
+++ include/utils/RandomNumberGenerator.h
@@ -7,7 +7,7 @@ namespace SDIZO {
         std::mt19937 generator;
         std::uniform_int_distribution<>* dist;
     public:
-        RandomNumberGenerator(int min = 0, int max = 1000000);
+        RandomNumberGenerator(int min = 0, int max = 2147483646);
         ~RandomNumberGenerator();
         int nextInt();
     };
